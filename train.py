bf0b3223291171ec1dbab72bb7e3c8d5de84142b
vaidaabelkyte <vaidilutia@gmail.com>
2017-11-15 14:40:41 +0000

    # Implementing Gradient Descent Algorithm

--- train.py
+++ train.py
@@ -35,4 +35,7 @@ def train(filename):
     correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+    # Implementing Gradient Descent Algorithm
+    train_op = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(cross_entropy)
+
    
