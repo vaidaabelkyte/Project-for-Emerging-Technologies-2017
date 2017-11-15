4fae7eb2dba81643d632a74515a79e3a11774c97
vaidaabelkyte <vaidilutia@gmail.com>
2017-11-15 14:30:10 +0000

    initializing the model

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+#following the tuorial found in page    http://dataaspirant.com/2017/05/03/handwritten-digits-recognition-tensorflow-python/
+
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("model_data/", one_hot=True) #Download the MNIS
 import tensorflow as tf #Import Tensorflow
@@ -15,4 +17,9 @@ y_ = tf.placeholder(tf.float32, shape=[None, 10])
 # creating variables
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
+
+# initializing the model
+test = tf.matmul(x, W) + b
+y = tf.nn.softmax(test)
+
 
