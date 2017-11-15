82824bf34430c2cf85411b363043f0399c3c78cf
vaidaabelkyte <vaidilutia@gmail.com>
2017-11-15 14:22:36 +0000

    Initializing parameters for the model

--- train.py
+++ train.py
@@ -2,3 +2,8 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("model_data/", one_hot=True) #Download the MNIS
 import tensorflow as tf #Import Tensorflow
 
+#Initializing parameters for the model
+BATCH = 100
+LEARNING_RATE = 0.01
+TRAINING_EPOCHS = 30
+
